cea2e51b0e45bc34476c51697205d69b5fed7af4
louis amstutz <louis.amstutz@moventes.com>
2019-01-25 16:57:55 +0100

    2.0.9

--- projects/firestore-dao/src/public_api.ts
+++ projects/firestore-dao/src/public_api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of firestore-dao
  */
 
+export * from './lib/decorators/enumerable.decorator';
 export * from './lib/models/abstract.model';
 export * from './lib/models/firestore.dao';
