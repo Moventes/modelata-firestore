568319282ae8f0e96516d26540e62a651455f290
louis amstutz <louis.amstutz@moventes.com>
2019-05-07 15:36:08 +0200

    fix collection path

--- projects/firestore-dao/src/lib/models/abstract.model.ts
+++ projects/firestore-dao/src/lib/models/abstract.model.ts
@@ -54,7 +54,9 @@ export abstract class AbstractModel {
       ObjectHelper.createHiddenProperty(this, 'id', dbObj['_id']);
     }
 
-    if (path) {
+    if (dbObj && dbObj['_collectionPath'] && !(<string>dbObj['_collectionPath']).includes('?') && (!path || !pathIds || pathIds.length === 0)) {
+      ObjectHelper.createHiddenProperty(this, 'collectionPath', dbObj['_collectionPath']);
+    } else if (path) {
       ObjectHelper.createHiddenProperty(this, 'collectionPath', ModelHelper.getPath(path, pathIds));
     } else if (dbObj && dbObj['_collectionPath']) {
       ObjectHelper.createHiddenProperty(this, 'collectionPath', dbObj['_collectionPath']);
